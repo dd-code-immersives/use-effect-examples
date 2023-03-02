7ba7f367b823c2023f4adffd97dfbbfc8d1dac4e
dd-code-immersives <domilamad89@gmail.com>
2023-03-02 14:23:33 -0500

    adding some useEffect examples

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+
+
+function Top() {
+  const [count, setCount] = useState(0);
+
+  //will run in response to the component Top() changing  
+  // in class based react is equivalent to DidComponentMount 
+  useEffect(() => {
+    console.log("Top rendered");
+  });
+
+  return (
+    <div>
+      <div onClick={() => setCount(count + 1)}>Top Level {count}</div>
+      <Middle />
+    </div>
+  );
+}
+
+function Middle() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log("Middle rendered");
+  });
+
+  return (
+    <div>
+      <div onClick={() => setCount(count + 1)}>Middle Level {count}</div>
+      <Bottom />
+    </div>
+  );
+}
+
+function Bottom() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log("Bottom rendered");
+  });
+
+  return <div onClick={() => setCount(count + 1)}>Bottom Level {count}</div>;
+}
+
+
+
+
+
+
 
 function App() {
+
+  //run when component is mounted 
+  // aka after it is rendered
+  useEffect(() => {
+    console.log('mounted');
+    return () => console.log('unmounting...');
+  }, [])  // <-- add this empty array here
+  
+
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Top/>
+      <Middle/>
+      <Bottom/>
     </div>
   );
 }
